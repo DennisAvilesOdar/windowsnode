574f38449e14c8559671651ccc46c640702dd4f0
Dennis Aviles Odar <dennis.aviles@icloud.com>
2016-11-18 15:51:28 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,34 @@ router.get('/api/v1/todos', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM articulo ORDER BY id nombre;');
+    const query = client.query('SELECT * FROM articulo ORDER BY nombre;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.post('/api/v1/todos', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {email: req.body.email, clave: req.body.password};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from f_validar_sesion ($1,$2)',
+    [data.email, data.clave]);
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
