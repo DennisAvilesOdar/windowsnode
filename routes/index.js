ede36e725c685e7a408a34322e76c1fe270b80a6
Dennis Mauricio Aviles Odar <dennismauricioavilesodar@MacBook-Pro-de-Dennis.local>
2016-11-28 01:36:36 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -70,4 +70,36 @@ router.post('/api/v1/inicio', (req, res, next) => {
   });
 });
 
+router.post('/api/v1/panel', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  const results = [];
+  // Grab data from http request
+  const data = {dni: req.body.dni};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select dni,(nombres || ' ' || apellido_paterno || ' ' ||apellido_materno)as nombre, email from personal where dni = $1 ',
+    [data.dni]);
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
 module.exports = router;
